79645f6931a4701c4b319e33a24c196139419de3
Felix Benning <felix.benning@gmail.com>
2020-11-03 15:54:29 +0100

    junction implementation

--- src/homework_2.jl
+++ src/homework_2.jl
@@ -87,10 +87,11 @@ time_horizon: calculate flow until time_horizon
 function discrete_godunov_solve(
 	flux, start_flux, stop_flux, start, stop, discr, density_0, time_horizon
 )
-	interval_borders = [(discr[idx]+discr[idx+1])/2 for idx in 1:(length(discr)-1)]
-	prepend!(interval_borders, start)
-	append!(interval_borders, stop)
-	interval_lengths = diff(interval_borders)
+	borders = Array{Float64,1}(undef, length(discr)+1)
+	borders[2:end-1] = [(discr[idx]+discr[idx+1])/2 for idx in 1:(length(discr)-1)]
+	borders[1] = start
+	borders[end] = stop
+	interval_lengths = diff(borders)
 	
 	u_den = unique(density_0)
 	abs_derivatives = abs.(flux'.(u_den))
@@ -172,7 +173,10 @@ idx_of_t(t) = findfirst(x->x>=t,times)
 begin
 	p = plot()
 	for t in 0:time_horizon/3-0.00001:time_horizon
-		plot!(discr, density[:,idx_of_t(t)], linetype=:steppost, label="t=$(@sprintf "%0.2f" t)" )
+		plot!(
+			discr, density[:,idx_of_t(t)];
+			linetype=:steppost, label="t=$(@sprintf "%0.2f" t)" 
+		)
 	end
 	p
 end
@@ -180,12 +184,99 @@ end
 # ╔═╡ 9be3f732-1c72-11eb-0e6f-a1d96e46797a
 md"# Exercise2"
 
-# ╔═╡ a399febe-1c72-11eb-05ad-85a8182dde8a
+# ╔═╡ 26307230-1dd9-11eb-2d5c-691039ab0dde
+abstract type Junction end
+
+# ╔═╡ 66da2a4e-1db2-11eb-2c28-a3b282c77228
+struct Road
+	flux
+	flux_max::Float64
+	discretization::Array{Float64,1}
+	interval_lengths::Array{Float64,1}
+	density::Array{Float64,2}
+	entrance::Junction
+	exit::Junction
+end
+
+# ╔═╡ 3f95ccb0-1dda-11eb-245d-03b638c1c4eb
+function demand(road::Road, time_idx)
+	rho = road.density[end,time_idx]
+	if rho < road.flux_max
+		return road.flux(rho)
+	else
+		return road.flux(road.flux_max)
+	end
+end
+
+# ╔═╡ 6abbd4b0-1ddb-11eb-027a-7762903061f3
+function supply(road::Road, time_idx)
+	rho = road.density[1,time_idx]
+	if rho < road.flux_max
+		return road.flux(road.flux_max)
+	else
+		return road.flux(rho)
+	end
+end
+
+# ╔═╡ 7cc675c0-1dd6-11eb-240e-3193d1ace5ed
+struct OneTwoJunction <: Junction
+	entrances::Array{Road,1}
+	exits::Array{Road,1}
+	distribution_matrix::Array{Float64,2}
+end
+
+# ╔═╡ b7bff5b0-1de1-11eb-38f4-8be3fcb3424d
+struct JunctionFlux
+	entrances::Array{Float64,1}
+	exits::Array{Float64,1}
+end
+
+# ╔═╡ e457cc40-1dd9-11eb-1388-75ea1e06bca7
+function junction_flux(junction::OneTwoJunction, time)
+	d = [demand(road, time) for road in junction.entrances]
+	s = [supply(road, time) for road in junction.exits]
+	
+	entrance_flux = Array{Float64,1}(undef, length(junction.entrances))
+	
+	#abuse one-to-two special case knowledge for solution
+	entrance_flux[1] = min(d[1], minimum(s ./ junction.distribution_matrix[:,1]))
+	exit_flux = junction.distribution_matrix[:,1] .* entrance_flux[1]
+	##
+	
+	return JunctionFlux(entrance_flux, exit_flux)
+end
+
+# ╔═╡ efbd74b0-1de1-11eb-2875-d5e772b9625d
+struct TwoOneJunction <: Junction
+	entrances::Array{Road,1}
+	exits::Array{Road,1}
+	right_of_way::Float64
+end
+
+# ╔═╡ 431e84a0-1de2-11eb-33ba-b9abe149f86f
+function junction_flux(junction::TwoOneJunction, time)
+	d = [demand(road, time) for road in junction.entrances]
+	s = [supply(road, time) for road in junction.exits]
+	
+	# abuse two-to-one special case knowledge for solution (Right of way priority)
+	q = junction.right_of_way
+	maximal_flow = min(d[1]+d[2], s[1])
+	
+	entrance_flux[1] = min(d[1], q/(1-q) * d[2], q*maximal_flow)
+	entrance_flux[2] = (1-q)/q * entrance_flux[1]
+	
+	exit_flux = sum(entrance_flux)
+	##
+	
+	return JunctionFlux(entrance_flux, exit_flux)
+end
+
+# ╔═╡ 7c5be130-1de3-11eb-14c4-21e5ef54a48a
 
 
 # ╔═╡ Cell order:
-# ╟─1f4955c0-1b9c-11eb-2e53-c10ebcb1ff07
 # ╠═d456a4d0-1c33-11eb-3c70-599cd64c2c83
+# ╟─1f4955c0-1b9c-11eb-2e53-c10ebcb1ff07
 # ╠═41175b30-1c3b-11eb-10d5-91414cfbb583
 # ╠═216e96c0-1c4c-11eb-0ae0-d7af7fc2f12e
 # ╠═430325e0-1b9c-11eb-26f7-93813a1b9698
@@ -201,4 +292,13 @@ md"# Exercise2"
 # ╠═6aaa0640-1c6e-11eb-31dd-d7223b7df72a
 # ╠═11073d10-1c5f-11eb-078b-714e16d80478
 # ╟─9be3f732-1c72-11eb-0e6f-a1d96e46797a
-# ╠═a399febe-1c72-11eb-05ad-85a8182dde8a
+# ╠═26307230-1dd9-11eb-2d5c-691039ab0dde
+# ╠═3f95ccb0-1dda-11eb-245d-03b638c1c4eb
+# ╠═6abbd4b0-1ddb-11eb-027a-7762903061f3
+# ╠═66da2a4e-1db2-11eb-2c28-a3b282c77228
+# ╠═7cc675c0-1dd6-11eb-240e-3193d1ace5ed
+# ╠═b7bff5b0-1de1-11eb-38f4-8be3fcb3424d
+# ╠═e457cc40-1dd9-11eb-1388-75ea1e06bca7
+# ╠═efbd74b0-1de1-11eb-2875-d5e772b9625d
+# ╠═431e84a0-1de2-11eb-33ba-b9abe149f86f
+# ╠═7c5be130-1de3-11eb-14c4-21e5ef54a48a
